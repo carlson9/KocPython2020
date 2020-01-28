86d9a6a83a202199d44f22b878bdf441bf2ca714
carlson9 <carlson.david@wustl.edu>
2020-01-28 15:44:52 +0300

    day2

---
+++ in-classMaterial/day2/lab1.py
@@ -0,0 +1,34 @@
+def binarify(num): 
+  """convert positive integer to base 2"""
+  if num<=0: return '0'
+  digits = []
+  return ''.join(digits)
+
+def int_to_base(num, base):
+  """convert positive integer to a string in any base"""
+  if num<=0:  return '0' 
+  digits = []
+  return ''.join(digits)
+
+def base_to_int(string, base):
+  """take a string-formatted number and its base and return the base-10 integer"""
+  if string=="0" or base <= 0 : return 0 
+  result = 0 
+  return result 
+
+def flexibase_add(str1, str2, base1, base2):
+  """add two numbers of different bases and return the sum"""
+  result = int_to_base(tmp, base1)
+  return result 
+
+def flexibase_multiply(str1, str2, base1, base2):
+  """multiply two numbers of different bases and return the product"""
+  result = int_to_base(tmp, base1)
+  return result 
+
+def romanify(num):
+  """given an integer, return the Roman numeral version"""
+  result = ""
+  return result
+  
+
