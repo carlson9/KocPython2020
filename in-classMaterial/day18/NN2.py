b1b1512e5fbdd46566bfc53052016d2cca7dd290
carlson9 <carlson.david@wustl.edu>
2020-05-12 14:18:52 +0300

    day 18 - NNs

---
+++ in-classMaterial/day18/NN2.py
@@ -0,0 +1,522 @@
+import random
+
+import numpy as np
+
+class Network(object):
+
+    def __init__(self, sizes):
+        """The list ``sizes`` contains the number of neurons in the
+        respective layers of the network.  For example, if the list
+        was [2, 3, 1] then it would be a three-layer network, with the
+        first layer containing 2 neurons, the second layer 3 neurons,
+        and the third layer 1 neuron.  The biases and weights for the
+        network are initialized randomly, using a Gaussian
+        distribution with mean 0, and variance 1.  Note that the first
+        layer is assumed to be an input layer, and by convention we
+        won't set any biases for those neurons, since biases are only
+        ever used in computing the outputs from later layers."""
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def feedforward(self, a):
+        """Return the output of the network if ``a`` is input."""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+        return a
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta,
+            test_data=None):
+        """Train the neural network using mini-batch stochastic
+        gradient descent.  The ``training_data`` is a list of tuples
+        ``(x, y)`` representing the training inputs and the desired
+        outputs.  The other non-optional parameters are
+        self-explanatory.  If ``test_data`` is provided then the
+        network will be evaluated against the test data after each
+        epoch, and partial progress printed out.  This is useful for
+        tracking progress, but slows things down substantially."""
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print("Epoch {0}: {1} / {2}".format(
+                    j, self.evaluate(test_data), n_test))
+            else:
+                print("Epoch {0} complete".format(j))
+
+    def update_mini_batch(self, mini_batch, eta):
+        """Update the network's weights and biases by applying
+        gradient descent using backpropagation to a single mini batch.
+        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
+        is the learning rate."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+        """Return a tuple ``(nabla_b, nabla_w)`` representing the
+        gradient for the cost function C_x.  ``nabla_b`` and
+        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+        to ``self.biases`` and ``self.weights``."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * \
+            sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        # Note that the variable l in the loop below is used a little
+        # differently to the notation in Chapter 2 of the book.  Here,
+        # l = 1 means the last layer of neurons, l = 2 is the
+        # second-last layer, and so on.  It's a renumbering of the
+        # scheme in the book, used here to take advantage of the fact
+        # that Python can use negative indices in lists.
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+    def evaluate(self, test_data):
+        """Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation."""
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        """Return the vector of partial derivatives \partial C_x /
+        \partial a for the output activations."""
+        return (output_activations-y)
+
+#### Miscellaneous functions
+def sigmoid(z):
+    """The sigmoid function."""
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
+    
+"""
+mnist_loader
+~~~~~~~~~~~~
+
+A library to load the MNIST image data.  For details of the data
+structures that are returned, see the doc strings for ``load_data``
+and ``load_data_wrapper``.  In practice, ``load_data_wrapper`` is the
+function usually called by our neural network code.
+"""
+
+import pickle as cPickle
+import gzip
+
+
+def load_data():
+    """Return the MNIST data as a tuple containing the training data,
+    the validation data, and the test data.
+
+    The ``training_data`` is returned as a tuple with two entries.
+    The first entry contains the actual training images.  This is a
+    numpy ndarray with 50,000 entries.  Each entry is, in turn, a
+    numpy ndarray with 784 values, representing the 28 * 28 = 784
+    pixels in a single MNIST image.
+
+    The second entry in the ``training_data`` tuple is a numpy ndarray
+    containing 50,000 entries.  Those entries are just the digit
+    values (0...9) for the corresponding images contained in the first
+    entry of the tuple.
+
+    The ``validation_data`` and ``test_data`` are similar, except
+    each contains only 10,000 images.
+
+    This is a nice data format, but for use in neural networks it's
+    helpful to modify the format of the ``training_data`` a little.
+    That's done in the wrapper function ``load_data_wrapper()``, see
+    below.
+    """
+    with gzip.open('mnist.pkl.gz','rb') as ff :
+        u = cPickle._Unpickler( ff )
+        u.encoding = 'latin1'
+        train, val, test = u.load()
+    return (train, val, test)
+
+def load_data_wrapper():
+    """Return a tuple containing ``(training_data, validation_data,
+    test_data)``. Based on ``load_data``, but the format is more
+    convenient for use in our implementation of neural networks.
+
+    In particular, ``training_data`` is a list containing 50,000
+    2-tuples ``(x, y)``.  ``x`` is a 784-dimensional numpy.ndarray
+    containing the input image.  ``y`` is a 10-dimensional
+    numpy.ndarray representing the unit vector corresponding to the
+    correct digit for ``x``.
+
+    ``validation_data`` and ``test_data`` are lists containing 10,000
+    2-tuples ``(x, y)``.  In each case, ``x`` is a 784-dimensional
+    numpy.ndarry containing the input image, and ``y`` is the
+    corresponding classification, i.e., the digit values (integers)
+    corresponding to ``x``.
+
+    Obviously, this means we're using slightly different formats for
+    the training data and the validation / test data.  These formats
+    turn out to be the most convenient for use in our neural network
+    code."""
+    tr_d, va_d, te_d = load_data()
+    training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
+    training_results = [vectorized_result(y) for y in tr_d[1]]
+    training_data = zip(training_inputs, training_results)
+    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
+    validation_data = zip(validation_inputs, va_d[1])
+    test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
+    test_data = zip(test_inputs, te_d[1])
+    return (training_data, validation_data, test_data)
+
+def vectorized_result(j):
+    """Return a 10-dimensional unit vector with a 1.0 in the jth
+    position and zeroes elsewhere.  This is used to convert a digit
+    (0...9) into a corresponding desired output from the neural
+    network."""
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
+    
+import os
+os.chdir('KocPython2020/in-classMaterial/day18')
+
+training_data, validation_data, test_data = load_data_wrapper()    
+
+training_data, validation_data, test_data = list(training_data), list(validation_data), list(test_data)
+
+net = Network([784, 30, 10])
+
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+#TODO: change the number of layers, the number of neurons per layer, the SGD parameters, etc.
+
+
+import pandas as pd
+
+# Location of dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+# Assign column names to the dataset
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+
+# Read dataset to pandas dataframe
+irisdata = pd.read_csv(url, names=names)
+
+irisdata.head()  
+
+# Assign data from first four columns to X variable
+X = irisdata.iloc[:, 0:4]
+
+# Assign data from first fifth columns to y variable
+y = irisdata.select_dtypes(include=[object]) 
+
+y.head()
+
+y.Class.unique()  
+
+from sklearn import preprocessing  
+le = preprocessing.LabelEncoder()
+
+y = y.apply(le.fit_transform)
+
+from sklearn.model_selection import train_test_split  
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)  
+
+
+from sklearn.preprocessing import StandardScaler  
+scaler = StandardScaler()  
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)  
+X_test = scaler.transform(X_test)
+
+from sklearn.neural_network import MLPClassifier  
+mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000)  
+mlp.fit(X_train, y_train.values.ravel()) 
+
+predictions = mlp.predict(X_test)  
+
+from sklearn.metrics import classification_report, confusion_matrix  
+print(confusion_matrix(y_test,predictions))  
+print(classification_report(y_test,predictions)) 
+
+#TODO: try to change the parameters to get a better score
+
+
+import pandas as pd
+wine = pd.read_csv('wine_data.csv', names = ["Cultivator", "Alchol", "Malic_Acid", "Ash", "Alcalinity_of_Ash", "Magnesium", "Total_phenols", "Falvanoids", "Nonflavanoid_phenols", "Proanthocyanins", "Color_intensity", "Hue", "OD280", "Proline"])
+
+wine.head()
+
+wine.describe().transpose()
+
+wine.shape
+
+X = wine.drop('Cultivator',axis=1)
+y = wine['Cultivator']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+# Fit only to the training data
+scaler.fit(X_train)
+
+StandardScaler(copy=True, with_mean=True, with_std=True)
+
+# Now apply the transformations to the data:
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+mlp = MLPClassifier(hidden_layer_sizes=(13,13,13),max_iter=500)
+
+mlp.fit(X_train,y_train)
+
+predictions = mlp.predict(X_test)
+
+print(confusion_matrix(y_test,predictions))
+
+print(classification_report(y_test,predictions))
+
+#coefs_ is a list of weight matrices, where weight matrix at index i represents the weights between layer i and layer i+1.
+
+#intercepts_ is a list of bias vectors, where the vector at index i represents the bias values added to layer i+1.
+
+len(mlp.coefs_)
+
+len(mlp.coefs_[0])
+
+len(mlp.intercepts_[0])
+
+
+from sklearn.pipeline import make_pipeline
+
+from matplotlib import pyplot as plt
+
+%config InlineBackend.figure_format = 'retina' 
+plt.style.use('bmh')
+
+def make_data():
+    N = 2000
+    X = 0.5*np.random.normal(size=N)+0.35
+
+    Xt = 0.75*X-0.35
+    X = X.reshape((N,1))
+
+    Y = -(8 * Xt**2 + 0.1*Xt + 0.1) + 0.05 * np.random.normal(size=N)
+    Y = np.exp(Y) + 0.05 * np.random.normal(size=N)
+    Y /= max(np.abs(Y))
+    return X, Y
+
+np.random.seed(0)
+X, Y = make_data()
+
+from sklearn.metrics import mean_squared_error, r2_score
+
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.5, random_state=0)
+
+plt.plot(Xtest[:,0], Ytest, '.');
+
+from sklearn.linear_model import Ridge
+
+ridge = Ridge()
+ridge.fit(Xtrain, Ytrain)
+
+Yguess = ridge.predict(Xtest)
+
+plt.plot(Xtest[:,0], Ytest, '.')
+plt.plot(Xtest[:,0], Yguess, 'r.')
+
+mean_squared_error(Ytest, Yguess), r2_score(Ytest, Yguess)
+
+from sklearn.neural_network import MLPRegressor
+
+mlp = MLPRegressor(random_state=0, activation='relu', hidden_layer_sizes=16)
+
+mlp.fit(Xtrain, Ytrain)
+
+Yguess = mlp.predict(Xtest)
+
+plt.plot(Xtest[:,0], Ytest, '.')
+plt.plot(Xtest[:,0], Yguess, 'r.')
+
+mean_squared_error(Ytest, Yguess), r2_score(Ytest, Yguess)
+
+mlp = MLPRegressor(random_state=0, activation='tanh', hidden_layer_sizes=(16,8))
+
+mlp.fit(Xtrain, Ytrain)
+
+Yguess = mlp.predict(Xtest)
+
+plt.plot(Xtest[:,0], Ytest, '.')
+plt.plot(Xtest[:,0], Yguess, 'r.')
+
+mean_squared_error(Ytest, Yguess), r2_score(Ytest, Yguess)
+
+
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+plt.style.use('seaborn-poster')
+
+np.random.seed(0)
+x = 10 * np.random.rand(100)
+
+def model(x, sigma=0.3):
+    fast_oscillation = np.sin(5 * x)
+    slow_oscillation = np.sin(0.5 * x)
+    noise = sigma * np.random.randn(len(x))
+
+    return slow_oscillation + fast_oscillation + noise
+
+plt.figure(figsize = (12,10))
+y = model(x)
+plt.errorbar(x, y, 0.3, fmt='o')
+
+xfit = np.linspace(0, 10, 1000)
+
+# fit the model and get the estimation for each data points
+yfit = RandomForestRegressor(100, random_state=42).fit(x[:, None], y).predict(xfit[:, None])
+ytrue = model(xfit, 0)
+
+plt.figure(figsize = (12,10))
+plt.errorbar(x, y, 0.3, fmt='o')
+plt.plot(xfit, yfit, '-r', label = 'predicted', zorder = 10)
+plt.plot(xfit, ytrue, '-k', alpha=0.5, label = 'true model', zorder = 10)
+plt.legend()
+
+mse = mean_squared_error(ytrue, yfit)
+print(mse)
+
+mlp = MLPRegressor(hidden_layer_sizes=(200,200,200), max_iter = 2000, solver='lbfgs', \
+                   alpha=0.01, activation = 'tanh', random_state = 8)
+
+yfit = mlp.fit(x[:, None], y).predict(xfit[:, None])
+
+plt.figure(figsize = (12,10))
+plt.errorbar(x, y, 0.3, fmt='o')
+plt.plot(xfit, yfit, '-r', label = 'predicted', zorder = 10)
+plt.plot(xfit, ytrue, '-k', alpha=0.5, label = 'true model', zorder = 10)
+plt.legend()
+
+mse = mean_squared_error(ytrue, yfit)
+print(mse)
+
+
+
+from sklearn.svm import SVR
+
+# define your model
+svr = SVR(C=1000)
+
+# get the estimation from the model
+yfit = svr.fit(x[:, None], y).predict(xfit[:, None])
+
+# plot the results as above
+plt.figure(figsize = (12,10))
+plt.errorbar(x, y, 0.3, fmt='o')
+plt.plot(xfit, yfit, '-r', label = 'predicted', zorder = 10)
+plt.plot(xfit, ytrue, '-k', alpha=0.5, label = 'true model', zorder = 10)
+plt.legend()
+
+mse = mean_squared_error(ytrue, yfit)
+print(mse)
+
+
+tt = pd.read_csv('../day17/immSurvey.csv')
+tt.head()
+
+alphas = tt.stanMeansNewSysPooled
+sample = tt.textToSend
+
+from sklearn.feature_extraction.text import CountVectorizer
+vec = CountVectorizer()
+X = vec.fit_transform(sample)
+X
+
+pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
+
+#down-weighting frequent words; term frequency–inverse document frequency (TF–IDF), which weights the word counts by a measure of how often they appear in the documents
+from sklearn.feature_extraction.text import TfidfVectorizer
+vec = TfidfVectorizer()
+X = vec.fit_transform(sample)
+pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, alphas,
+random_state=1)
+
+mlp = MLPRegressor(random_state=0, activation='tanh', hidden_layer_sizes=(100,50))
+
+mlp.fit(Xtrain, ytrain)
+
+yguess = mlp.predict(Xtest)
+
+np.corrcoef(ytest, yguess)
+
+mlp = MLPRegressor(random_state=0, activation='tanh', hidden_layer_sizes=(1000,500))
+
+mlp.fit(Xtrain, ytrain)
+
+yguess = mlp.predict(Xtest)
+
+np.corrcoef(ytest, yguess)
+
+mlp = MLPRegressor(random_state=0, activation='tanh', hidden_layer_sizes=(50,25))
+
+mlp.fit(Xtrain, ytrain)
+
+yguess = mlp.predict(Xtest)
+
+np.corrcoef(ytest, yguess)
+
+mlp = MLPRegressor(random_state=0, activation='tanh', hidden_layer_sizes=(20,10))
+
+mlp.fit(Xtrain, ytrain)
+
+yguess = mlp.predict(Xtest)
+
+np.corrcoef(ytest, yguess)
+
+mlp = MLPRegressor(random_state=0, activation='tanh', hidden_layer_sizes=(10,5))
+
+mlp.fit(Xtrain, ytrain)
+
+yguess = mlp.predict(Xtest)
+
+np.corrcoef(ytest, yguess)
+
+#TODO: play around with number of layers, layer sizes, different activations, etc. see if you can do better
