508876a25ba13f074b39ae3823eaf9454af57695
carlson9 <carlson.david@wustl.edu>
2020-05-20 09:18:29 +0300

    day19 NNs and tensorflow

--- in-classMaterial/day18/NN2.py
+++ in-classMaterial/day18/NN2.py
@@ -274,6 +274,8 @@ print(classification_report(y_test,predictions))
 
 #TODO: try to change the parameters to get a better score
 
+import os
+os.chdir('KocPython2020/in-classMaterial/day18')
 
 import pandas as pd
 wine = pd.read_csv('wine_data.csv', names = ["Cultivator", "Alchol", "Malic_Acid", "Ash", "Alcalinity_of_Ash", "Magnesium", "Total_phenols", "Falvanoids", "Nonflavanoid_phenols", "Proanthocyanins", "Color_intensity", "Hue", "OD280", "Proline"])
@@ -287,6 +289,7 @@ wine.shape
 X = wine.drop('Cultivator',axis=1)
 y = wine['Cultivator']
 
+from sklearn.model_selection import train_test_split  
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 from sklearn.preprocessing import StandardScaler
@@ -302,11 +305,14 @@ StandardScaler(copy=True, with_mean=True, with_std=True)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
+from sklearn.neural_network import MLPClassifier  
 mlp = MLPClassifier(hidden_layer_sizes=(13,13,13),max_iter=500)
 
 mlp.fit(X_train,y_train)
 
 predictions = mlp.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix
 
 print(confusion_matrix(y_test,predictions))
 
